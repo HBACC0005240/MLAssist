7b09f903df8c35ea2a861668014e25f0a1712cce
wangzhuoqi <wangzhuoqi@tdwaji.com>
2022-02-28 21:27:57 +0800

    增加debug显示

--- MLAssistTool/MLDebugWgt.cpp
+++ MLAssistTool/MLDebugWgt.cpp
@@ -1,10 +1,11 @@
 #include "MLDebugWgt.h"
 #include "ITSignalProxy.h"
-MLDebugWgt::MLDebugWgt(QWidget *parent)
+MLDebugWgt::MLDebugWgt(QWidget* parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
-	connect(g_pSignalProxy,SIGNAL(signal_addOneDebugMsg(const QString&)),this,SLOT(doAddOneDebugMsg(const QString&)));
+	ui.plainTextEdit->setMaximumBlockCount(1000);
+	connect(g_pSignalProxy, SIGNAL(signal_addOneDebugMsg(const QString&)), this, SLOT(doAddOneDebugMsg(const QString&)));
 }
 
 MLDebugWgt::~MLDebugWgt()
@@ -13,6 +14,6 @@ MLDebugWgt::~MLDebugWgt()
 
 void MLDebugWgt::doAddOneDebugMsg(const QString& msg)
 {
-	if(ui.checkBox_enable->isChecked())
-		ui.textEdit->append(msg);
+	if (ui.checkBox_enable->isChecked())
+		ui.plainTextEdit->appendPlainText(msg);
 }
